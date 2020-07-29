3bff5ac21e0f325a77f611c9539fdd42f224b519
mark <mark@sansome.org>
2020-07-29 14:55:48 +0100

    Completed basic key management functionality

--- Crypto/PublicKey/PublicKeyTest.py
+++ Crypto/PublicKey/PublicKeyTest.py
@@ -8,7 +8,7 @@
 # Public Key Encryption / Decryption.
 
 # M. Sansome July 2020
-# Version 0.2
+# Version 0.3
 
 # Version History
 # ===============
@@ -16,12 +16,13 @@
 # v0.01 Created warning message and set preference file.
 # v0.1.0 Basic PK encrypt/decrypt implemented (hard-coded files and keys).
 # v0.2 Added the encrypt / decrypt mode option
+# v0.3 Completed basic key management functionality
 
 import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
 from tkinter import filedialog
-# from tkinter import messagebox
+from tkinter import messagebox
 # from random import shuffle
 # from threading import Thread
 import os
@@ -43,48 +44,59 @@ class App(tk.Tk):
             self.prefs["hide_warning"] = False
 
         if not self.prefs['hide_warning']:
-            self.show_warning_message()
+            self.show_warning_message_window()
+        self.priv_key_identifier = '_privkey.txt'
+        self.pub_key_identifier = '_pubkey.txt'
+        self.key_dir = 'PublicKey/'
+        self.pub_key_name = 'm_sansome'
+        self.priv_key_name = "m_sansome"
+        self.pub_key = self.pub_key_name + self.pub_key_identifier
+        self.priv_key = self.priv_key_name + self.priv_key_identifier
+        self.pub_keys = self.get_keys(self.pub_key_identifier, self.key_dir)
+        self.priv_keys = self.get_keys(self.priv_key_identifier, self.key_dir)
         self.title(title)
         self.setup_frames()
         self.frame_content()
+        self.pub_key_select()
 
     def setup_frames(self):
         # Create the top (input) frame:
         self.input_frame = tk.LabelFrame(self, text="Input Area")
-        self.input_frame.grid(row=0, column=0, columnspan=3,
+        self.input_frame.grid(row=0, column=0, columnspan=4,
                               padx=5, pady=5, sticky=tk.NSEW)
         self.input_frame.columnconfigure(0, weight=1)
         self.input_frame.rowconfigure(0, weight=1)
 
-        # Create the middle (alphabet) frame:
+        # Create the middle (Mode) frame:
         self.mode_frame = tk.LabelFrame(self, text="Mode")
         self.mode_frame.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
-        self.mode_frame.columnconfigure(0, weight=1)
         self.mode_frame.rowconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
+        self.mode_frame.columnconfigure(0, weight=1)
 
-        # Create the middle (Analysis tools) frame:
-        self.analysis_tools_frame = tk.LabelFrame(self, text="Analysis Tools")
-        self.analysis_tools_frame.grid(
-            row=1, column=1, padx=5, pady=5, sticky=tk.NW)
-        self.analysis_tools_frame.columnconfigure(0, weight=1)
-        self.analysis_tools_frame.rowconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
+        # Create the middle (Public Key Selection) frame:
+        self.pub_key_selection_frame = tk.LabelFrame(self, text="Public Key")
+        self.pub_key_selection_frame.grid(
+            row=1, column=1, padx=5, pady=5, sticky=tk.W)
+        self.pub_key_selection_frame.columnconfigure(0, weight=1)
+        self.pub_key_selection_frame.rowconfigure(0, weight=1)
 
-        # Create the middle (Key tools) frame:
-        self.key_tools_frame = tk.LabelFrame(self, text="Key Tools")
-        self.key_tools_frame.grid(
-            row=1, column=2, padx=5, pady=5, sticky=tk.NSEW)
-        self.key_tools_frame.columnconfigure(0, weight=1)
-        self.key_tools_frame.rowconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
+        # Create the middle (Private Key Selection) frame:
+        self.priv_key_selection_frame = tk.LabelFrame(self, text="Private Key")
+        self.priv_key_selection_frame.grid(
+            row=1, column=2, padx=5, pady=5, sticky=tk.W)
+        self.priv_key_selection_frame.columnconfigure(0, weight=1)
+        self.priv_key_selection_frame.rowconfigure(0, weight=1)
+
+        # Create the middle (New Key Pair) frame:
+        self.new_key_pair_frame = tk.LabelFrame(self, text="New Key Pair")
+        self.new_key_pair_frame.grid(
+            row=1, column=3, padx=5, pady=5, sticky=tk.E)
+        self.new_key_pair_frame.columnconfigure(0, weight=1)
+        self.new_key_pair_frame.rowconfigure(0, weight=1)
 
         # Create the bottom (output) frame:
         self.output_frame = tk.LabelFrame(self, text="Output Area")
-        self.output_frame.grid(row=2, column=0, columnspan=3,
+        self.output_frame.grid(row=2, column=0, columnspan=4,
                                padx=5, pady=5, sticky=tk.NSEW)
         self.output_frame.columnconfigure(0, weight=1)
         self.output_frame.rowconfigure(0, weight=1)
@@ -101,7 +113,7 @@ class App(tk.Tk):
                  text="Please type, paste, or load from file, the input text into this box:").grid(row=0, columnspan=3,
                                                                                                    sticky=tk.W)
         self.input_box = scrolledtext.ScrolledText(
-            self.input_frame, height=8, wrap=tk.WORD)
+            self.input_frame, height=12, wrap=tk.WORD)
         self.input_box.columnconfigure(0, weight=1)
         self.input_box.grid(row=1, column=0, columnspan=7, sticky=tk.NSEW)
         ttk.Button(self.input_frame,
@@ -113,58 +125,71 @@ class App(tk.Tk):
         ttk.Button(self.input_frame,
                    text="Encrypt/Decrypt",
                    command=self.enc_dec).grid(row=2, column=2, padx=5, pady=5, sticky=tk.E)
-        # ttk.Button(self.input_frame,
-        #            text="Auto Decrypt",
-        #            command=self.create_auto_decrypt_window).grid(row=2, column=4, padx=5, pady=5, sticky=tk.E)
 
         # Content for the Mode Frame:
         self.radio_mode = tk.StringVar()
         self.radio_mode.set("auto")
-        tk.Radiobutton(self.mode_frame, text="Encrypt", variable=self.radio_mode,
-                       command=self.enc_or_dec, value="encrypt").grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
-        tk.Radiobutton(self.mode_frame, text="Decrypt", variable=self.radio_mode,
-                       command=self.enc_or_dec, value="decrypt").grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
-        tk.Radiobutton(self.mode_frame, text="Auto Detect", variable=self.radio_mode,
-                       command=self.enc_or_dec, value="auto").grid(row=2, column=0, padx=5, pady=5,
-                                                                   sticky=tk.W)
+        ttk.Radiobutton(self.mode_frame, text="Encrypt", variable=self.radio_mode,
+                        command=self.enc_or_dec, value="encrypt").grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+        ttk.Radiobutton(self.mode_frame, text="Decrypt", variable=self.radio_mode,
+                        command=self.enc_or_dec, value="decrypt").grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+        ttk.Radiobutton(self.mode_frame, text="Auto Detect", variable=self.radio_mode,
+                        command=self.enc_or_dec, value="auto").grid(row=2, column=0, padx=5, pady=5,
+                                                                    sticky=tk.W)
 
-        # # Content for the analysis tools frame
-        # ttk.Button(self.analysis_tools_frame,
-        #            text="Index of Coincidence",
-        #            command=self.create_ic_window).grid(row=0, column=0, padx=2, pady=2, sticky=tk.NW)
-        # ttk.Button(self.analysis_tools_frame,
-        #            text="Frequency Analysis",
-        #            command=self.create_freq_analysis_window).grid(row=1, column=0, padx=2, pady=2, sticky=tk.NW)
-        # ttk.Button(self.analysis_tools_frame,
-        #            text="Doubles Analysis",
-        #            command=self.create_double_letters_window).grid(row=2, column=0, padx=2, pady=2, sticky=tk.NW)
+        # Content for the Public Keys Frame:
+        self.key_choice = tk.StringVar()
+        ttk.Label(self.pub_key_selection_frame, text="Please select key to use:",
+                  ).grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+        self.pub_key_combo = ttk.Combobox(self.pub_key_selection_frame,
+                                          textvariable=self.key_choice,
+                                          state="readonly", values=self.pub_keys)
+        self.pub_key_combo.current(
+            self.pub_keys.index(self.pub_key_name))
+        self.pub_key_combo.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+        self.pub_key_combo.bind("<<>ComboboxSelected>")
 
-        # # Content for the key tools frame
-        # ttk.Button(self.key_tools_frame,
-        #            text="Random Key",
-        #            command=self.random_alphabet).grid(row=0, column=0, padx=2, pady=2, sticky=tk.W)
-        # ttk.Button(self.key_tools_frame,
-        #            text="Copy Key to Clipboard",
-        #            command=self.copy_key_to_clipboard).grid(row=1, column=0, padx=2, pady=2, sticky=tk.W)
-        # ttk.Button(self.key_tools_frame,
-        #            text="Import Key",
-        #            command=self.create_key_import_window).grid(row=2, column=0, padx=2, pady=2, sticky=tk.W)
-        # ttk.Button(self.key_tools_frame,
-        #            text="Invert Key",
-        #            command=self.inv_key).grid(row=3, column=0, padx=2, pady=2, sticky=tk.W)
-        # ttk.Button(self.key_tools_frame,
-        #            text="Reset Key",
-        #            command=self.make_blank_alphabet).grid(row=4, column=0, padx=2, pady=2, sticky=tk.W)
+        # Content for the Private Keys Frame:
+        self.priv_key_choice = tk.StringVar()
+        ttk.Label(self.priv_key_selection_frame, text="Please select key to use:",
+                  ).grid(row=0, column=0, pady=5, sticky=tk.W)
+        self.priv_key_combo = ttk.Combobox(self.priv_key_selection_frame,
+                                           textvariable=self.priv_key_choice, state="readonly",
+                                           values=self.priv_keys)
+        self.priv_key_combo.current(
+            self.priv_keys.index(self.priv_key_name))
+        self.priv_key_combo.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+        self.priv_key_combo.bind("<<>ComboboxSelected>")
+
+        # Content for the New Key Pair Frame:
+        ttk.Button(self.new_key_pair_frame, text="Create a new Key Pair").grid(
+            row=0, column=0, padx=5, pady=5)
 
         # Content for the output frame, (one text box only).
         self.output_box = scrolledtext.ScrolledText(
-            self.output_frame, width=40, height=8, wrap=tk.WORD)
-        self.output_box.grid(row=0, column=0, columnspan=3, sticky=tk.NSEW)
+            self.output_frame, width=50, height=12, wrap=tk.WORD)
+        self.output_box.grid(row=0, column=0,
+                             columnspan=3, sticky=tk.NSEW)
         ttk.Button(self.output_frame,
                    text="Copy to Clipboard",
                    command=self.copy_output_to_clipboard).grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
-        # self.create_alphabet_entry_boxes()
-        # self.make_blank_alphabet()
+
+    def get_keys(self, key_identifier, key_dir):
+        strp = len(key_identifier)
+        keys = []
+        # keys = [file for file in os.listdir(key_dir) if file.endswith('privkey.txt')]
+        for file in os.listdir(key_dir):
+            if file.endswith(key_identifier):
+                keys.append(file[:-strp])
+        return keys
+
+    def pub_key_select(self):
+        selected = self.pub_key_combo.get()
+        self.pub_key = selected + self.pub_key_identifier
+
+    def priv_key_select(self):
+        selected = self.priv_key_combo.get()
+        self.priv_key = selected + self.priv_key_identifier
 
     def save_prefs(self, filename):
         with open(filename, 'wb') as handle:
@@ -174,51 +199,68 @@ class App(tk.Tk):
         with open(filename, 'rb') as handle:
             self.prefs = pickle.load(handle)
 
-    def pub_key_warning_close(self):
-        print("Hide Warning is set to:", self.hide_warning.get())
-        self.prefs["hide_warning"] = self.hide_warning.get()
-        self.save_prefs(self.prefs_filename)
-        self.pub_key_warning_window.destroy()
-
     def enc_or_dec(self):
         self.mode = self.radio_mode.get()
         if self.radio_mode.get() == "auto":
-            self.radio_mode.set("encrypt")
-            self.auto_detect_mode()
+            self.detect_mode()
+        self.update_the_combos()
 
-    def auto_detect_mode(self):
+    def detect_mode(self):
         # Automatically determine whether to encrypt or decrypt based
         # on what's in the input box.
         # If it's largely alphabetic characters - Encrypt
         # Encrypted messages have the message length, blocksize and the
         # encrypted message itself (which is all numeric) separated by
         # the "_" symbol at the start of the text.
+        self.mode = self.radio_mode.get()
+        if self.radio_mode.get() == "auto":
+            self.radio_mode.set("encrypt")
+            self.mode = "encrypt"
         message = self.input_box.get(0.0, tk.END)
         message = message.strip()
         message_part1 = message[:10]
         message_part2 = message[11:500]
         if any(x.isalpha() for x in message):  # If there are alphabetic characters Encrypt
             self.radio_mode.set("encrypt")
+            self.mode = "encrypt"
         elif "_" in message_part1 and message_part2.isdigit():
             self.radio_mode.set("decrypt")
+            self.mode = "decrypt"
         else:
             self.radio_mode.set("encrypt")  # If in doubt - set to encrypt
+            self.mode = "encrypt"
+        self.update_the_combos()
 
     def enc_dec(self):
+        # self.detect_mode()
+        self.pub_key_select()
+        self.priv_key_select()
         filename = 'output.txt'
         message = self.input_box.get(0.0, tk.END)
         if self.mode == 'encrypt':
-            pubkeyFilename = 'PublicKey/m_sansome_pubkey.txt'
-            encrypedText = publicKeyCipherASCII.encryptAndWriteToFile(filename, pubkeyFilename,
-                                                                      message)
+            # pubkeyFilename = 'PublicKey/m_sansome_pubkey.txt'
+            encrypedText = publicKeyCipherASCII.encryptAndWriteToFile(filename,
+                                                                      (self.key_dir + "/" + self.pub_key), message)
             self.output_box.delete(0.0, tk.END)
             self.output_box.insert(0.0, encrypedText)
         elif self.mode == 'decrypt':
-            privKeyFilename = 'PublicKey/m_sansome_privkey.txt'
-            decryptedText = publicKeyCipherASCII.messageDecrypt(
-                message, privKeyFilename)
-            self.output_box.delete(0.0, tk.END)
-            self.output_box.insert(0.0, decryptedText)
+            # privKeyFilename = 'PublicKey/m_sansome_privkey.txt'
+            try:
+                decryptedText = publicKeyCipherASCII.messageDecrypt(
+                    message, (self.key_dir + self.priv_key))
+                self.output_box.delete(0.0, tk.END)
+                self.output_box.insert(0.0, decryptedText)
+            except:
+                tk.messagebox.showerror(
+                    "Error", "Hmmm... Have you used the wrong key?")
+
+    def update_the_combos(self):
+        if self.mode == "decrypt":
+            self.pub_key_combo.configure(state="disabled")
+            self.priv_key_combo.configure(state="readonly")
+        if self.mode == "encrypt":
+            self.priv_key_combo.configure(state="disabled")
+            self.pub_key_combo.configure(state="readonly")
 
     def clear_input(self):
         self.input_box.delete(0.0, tk.END)
@@ -226,13 +268,17 @@ class App(tk.Tk):
 
     def fileDialog(self):
         # Method which invokes the import file dialogue
+        self.clear_input()  # Empty the input box first
         self.filename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select A File", filetypes=[
             ("TXT", "*.txt"),
             ("All files", "*")])
-        with open(self.filename) as input_file:
-            self.input_box.delete(0.0, tk.END)
-            self.input_box.insert(0.0, input_file.read())
-        self.auto_detect_mode()
+        try:
+            with open(self.filename) as input_file:
+                self.input_box.delete(0.0, tk.END)
+                self.input_box.insert(0.0, input_file.read())
+        except (FileNotFoundError, TypeError):  # If you hit "cancel" you get an error
+            pass
+        self.detect_mode()  # Once loaded - detect what type of file it is.
 
     def copy_to_clipboard(self, output):
         # Main Copy to clipboard method (takes the desired item as a parameter)
@@ -245,7 +291,7 @@ class App(tk.Tk):
         out_txt = self.output_box.get(0.0, tk.END)
         self.copy_to_clipboard(out_txt)
 
-    def show_warning_message(self):
+    def show_warning_message_window(self):
         self.pub_key_warning_window = tk.Toplevel(self)
         self.pub_key_warning_window.title("Warning!")
         self.pub_key_warning_window.attributes("-topmost", True)
@@ -279,8 +325,14 @@ class App(tk.Tk):
         # warn_close_button = ttk.Button(self.warning_frame, text="Close",
         #                                command=lambda: self.pub_key_warning_window.destroy())
         warn_close_button = ttk.Button(self.warning_frame, text="Close",
-                                       command=self.pub_key_warning_close)
+                                       command=self.close_warning_message_window)
         warn_close_button.pack(side=tk.RIGHT)
+
+    def close_warning_message_window(self):
+        print("Hide Warning is set to:", self.hide_warning.get())
+        self.prefs["hide_warning"] = self.hide_warning.get()
+        self.save_prefs(self.prefs_filename)
+        self.pub_key_warning_window.destroy()
 
 
 if __name__ == "__main__":
